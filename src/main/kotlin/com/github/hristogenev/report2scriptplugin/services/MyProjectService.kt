00bad8391182dc396e4f4c5ce8159550879a6c60
GitHub Action <action@github.com>
2024-08-26 12:35:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/hristogenev/report2scriptplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/hristogenev/report2scriptplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.hristogenev.report2scriptplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hristogenev.report2scriptplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
